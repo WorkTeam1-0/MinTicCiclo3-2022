715b815b76513af3a44754e47089107a08c7d819
JulianRosero0000 <julian.rosero0000@gmail.com>
2022-08-26 11:06:04 -0500

    se crean los constructores y los getters y setters

--- workTeam1.0V1-master/src/main/java/com/sprint2/workTeam/entity/Perfil.java
+++ workTeam1.0V1-master/src/main/java/com/sprint2/workTeam/entity/Perfil.java
@@ -7,6 +7,46 @@ public class Perfil {
     //Atributo
     private String id;
     private String imagen;
+
+    public Perfil(String id, String imagen, String telefono, User usuario) {
+        this.id = id;
+        this.imagen = imagen;
+        this.telefono = telefono;
+        this.usuario = usuario;
+    }
+
     private String telefono;
     private User usuario;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
 }
--- workTeam1.0V1-master/src/main/java/com/sprint2/workTeam/entity/Transaccion.java
+++ workTeam1.0V1-master/src/main/java/com/sprint2/workTeam/entity/Transaccion.java
@@ -8,6 +8,55 @@ public class Transaccion {
     private Long id;
     private String concepto;
     private Float monto;
+
+    public Transaccion(Long id, String concepto, Float monto, User usuario, Empresa empresa) {
+        this.id = id;
+        this.concepto = concepto;
+        this.monto = monto;
+        this.usuario = usuario;
+        this.empresa = empresa;
+    }
+
     private User usuario;
     private Empresa empresa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public Float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Float monto) {
+        this.monto = monto;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
 }
